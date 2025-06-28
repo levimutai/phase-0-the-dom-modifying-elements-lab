a52417bf13642b70e61b2b5a449171dc1752c970
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-06-28 14:38:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here!// Remove the 'main' node from the DOM
+const main = document.getElementById('main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = "Levi Mutai is the champion";
+
+
+document.body.append(newHeader);
